567a4c1af58a5c0e498d45064ab23406d885cbde
juliogersg <jseminariogastelo@gmail.com>
2021-03-17 21:56:31 -0500

    Dia 3 - Semana 5

--- 5. Semana 5/2. Dia 2/02-localstorage2.js
+++ 5. Semana 5/2. Dia 2/02-localstorage2.js
@@ -10,6 +10,12 @@ formulario.onsubmit = function (e) {
     if (inputTitulo.value.trim() === '' ||
         inputDescripcion.value.trim() === '') {
         // Mostrar error
+        Swal.fire({
+            icon: 'error',
+            title: 'Ups!',
+            text: 'Todos los campos deben estar debidamente llenados',
+            // timer: 3000
+        });
         return;
     }
 
@@ -19,6 +25,17 @@ formulario.onsubmit = function (e) {
     }
 
     arrayTODOS.push(objTODO);
+
+    // guardar el arreglo en el localStorage
+    /**
+     * JSON.stringify(objJSON)
+     * retorna un string que viene a ser el objeto JSON en
+     * formato STRING
+     */
+    let arregloSTRING = JSON.stringify(arrayTODOS);
+    localStorage.setItem('todos', arregloSTRING);
+    // localStorage solo recibe strings
+
     this.reset();
     // this no se puede usar con funciones flecha
     dibujarTODOS();
@@ -27,39 +44,60 @@ formulario.onsubmit = function (e) {
 /**
  * Funcion que toma el arrayTODOS y lo dibuja en el tbody
  */
-const dibujarTODOS = ()=>{
+const dibujarTODOS = () => {
     // Limpiar el tbody antes de empezar
     tbody.innerHTML = '';
     // Comienzo de la funcion para agregar a la tabla
-    arrayTODOS.forEach((objTODO,i)=>{
-    const tr = document.createElement('tr');
-    const tdNro = document.createElement('td');
-    const tdTarea = document.createElement('td');
-    const tdAcciones = document.createElement('td');
-    const btnEliminar = document.createElement('button');
+    arrayTODOS.forEach((objTODO, i) => {
+        const tr = document.createElement('tr');
+        const tdNro = document.createElement('td');
+        const tdTarea = document.createElement('td');
+        const tdAcciones = document.createElement('td');
+        const btnEliminar = document.createElement('button');
 
-    tdNro.innerText = i+1;
-    tdTarea.innerHTML = /*html */
-        `<h3>${objTODO.titulo}</h3>
+        tdNro.innerText = i + 1;
+        tdTarea.innerHTML = /*html */
+            `<h3>${objTODO.titulo}</h3>
         <p>${objTODO.descripcion}</p>`;
-    btnEliminar.innerText = 'Eliminar';
-    // Configurando el evento click para cada boton eliminar
-    btnEliminar.onclick = ()=>{
-        eliminarTODO(i);
-    }
-    tdAcciones.appendChild(btnEliminar);
+        btnEliminar.innerText = 'Eliminar';
+        // Configurando el evento click para cada boton eliminar
+        btnEliminar.onclick = () => {
+            eliminarTODO(i);
+        }
+        tdAcciones.appendChild(btnEliminar);
 
-    tr.appendChild(tdNro);
-    tr.appendChild(tdTarea);
-    tr.appendChild(tdAcciones);
+        tr.appendChild(tdNro);
+        tr.appendChild(tdTarea);
+        tr.appendChild(tdAcciones);
 
-    tbody.appendChild(tr);
+        tbody.appendChild(tr);
     })
 }
 
-const eliminarTODO = (posicion)=>{
+const eliminarTODO = (posicion) => {
     // array.splice(posicion,nro_elementos)
     // Elimina 'nro_elementos" de un arreglo a partir de la posicion 'posicion'
-    arrayTODOS.splice(posicion,1);
+    arrayTODOS.splice(posicion, 1);
+    // actualizar el localStorage
+    localStorage.setItem('todos', JSON.stringify(arrayTODOS));
     dibujarTODOS();
 }
+
+(() => {
+    if (localStorage.getItem('todos')) {
+        /**
+         * JSON.parse(string-en-formato-json)
+         * Devuelve un JSON como tal dado un string
+         * (conversion inversa a stringify)
+         */
+        arrayTODOS = JSON.parse(localStorage.getItem('todos'));
+        dibujarTODOS();
+    }
+    // configurando WAVES.JS
+    // se agrega la funcion de waves al btnSubmit
+    Waves.attach('#btnSubmit',['waves-button','waves-float']);
+    // Waves.attach('#inputTitulo',['waves-button','waves-float']);
+    // Waves.attach('#inputDescripcion',['waves-button','waves-float']);
+
+    Waves.init();
+})()
